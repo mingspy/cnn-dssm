ed9a5fdedd232d4b8ca02c3e537585951507e1f1
xiulei li <mingspy@163.com>
2017-11-08 14:07:03 +0800

    Add files via upload

--- predict.py
+++ predict.py
@@ -6,11 +6,15 @@ from data_helpers import *
 from datetime import datetime
 from scipy.spatial.distance import cosine
 import json
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 def load_vocab():
+    global args
     vocab = []
     vocab_idx = {}
-    inf = open('../../data/dssm/dssm_voc.txt','r')
+    inf = open(args.vocab_path,'r')
     cnt = 0 
     for line in inf:
         c = line.strip('\n')
@@ -23,14 +27,40 @@ def load_vocab():
 
 
 def predict(graph, query_in,query_doc):
-    batch_size = 50
+    def test_phrases_sim(sess,query_doc,vocab_idx):
+        test_fname = './test_phrases.txt'
+        if not os.path.exists(test_fname): return 
+        inf = open(test_fname,'r')
+        for line in inf:
+            line = line.strip()
+            test_phrases = [i.decode('utf-8') for i in line.split('|')]
+            test_vecs = []
+            for p in test_phrases:
+                x = [1]
+                for i in p:
+                    if i in vocab_idx: x.append(int(vocab_idx[i]))
+                x = x[:args.sentence_len - 2]
+                x.append(2)
+                r = args.sentence_len - len(x)
+                for i in xrange(r):
+                    x.append(0)
+                test_vecs.append(x)
+                pass
+            test_vecs = sess.run(query_doc, feed_dict={query_in:test_vecs})
+            print test_phrases[0],':',
+            for i,w in enumerate(test_phrases):
+                if i == 0: continue
+                print  w, 1 - cosine(test_vecs[0],test_vecs[i]),
+            print '\n---------------------'
+        pass
+    batch_size = 200
     vocab,vocab_idx = load_vocab()
 
     config = new_gpu_config()
 
     with tf.Session(graph=graph, config=config) as sess:
         print datetime.now(), 'init batches'
-        batches = load_test_dssm_data('../../data/dssm/test.txt', args.sentence_len)
+        batches = load_test_dssm_data(args.test_path, args.sentence_len)
         print datetime.now(), 'init batches end'
         X = []
         Y = []
@@ -40,7 +70,6 @@ def predict(graph, query_in,query_doc):
             x_batch, y_batch = batches[0][batch*batch_size:(batch+1) * batch_size],batches[1][batch*batch_size:(batch+1) * batch_size]
             title_vecs = sess.run(query_doc, feed_dict={query_in:x_batch})
             doc_vecs = sess.run(query_doc, feed_dict={query_in:y_batch})
-            print type(title_vecs),title_vecs
             X += title_vecs.tolist()
             Y += doc_vecs.tolist()
             print( 'len X %d'% len(X))
@@ -51,8 +80,11 @@ def predict(graph, query_in,query_doc):
                     if i == 0: break
                     t.append(vocab[i])
                 titles.append(''.join(t))
+
+        test_phrases_sim(sess,query_doc,vocab_idx)
+
     urls = []
-    inf = open('../../data/dssm/test.txt','r')
+    inf = open(args.test_path,'r')
     for line in inf:
         line = line.strip()
         if not line: continue
@@ -116,9 +148,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--model_dir", default="./model_yarn/", type=str, help="Frozen model file to import")  
 parser.add_argument("--frozen_model_name", default="frozen_dssm.pb", type=str, help="Frozen model file to import")  
 parser.add_argument("--sentence_len", default="1000", type=int, help="Frozen model file to import")  
+parser.add_argument("--vocab_path", default="../../data/dssm/dssm_voc.txt", type=str, help="vocab")  
+parser.add_argument("--test_path", default="../../data/dssm/test.txt", type=str, help="vocab")  
 args = parser.parse_args()  
 
-set_gpu_devices('1')
+set_gpu_devices('2')
 
 freeze_graph(args.model_dir,frozen_model_name = args.frozen_model_name)
 graph = load_graph(os.path.join(args.model_dir,args.frozen_model_name))
--- start_locally.sh
+++ start_locally.sh
@@ -16,16 +16,24 @@
 #word2vec -train train.txt -output vec.txt -size 256 -sample 1e-4 -window 7 -threads 10 -negative 5 -hs 1 -binary 0 -iter 100
 if [ $1 = train ]; then
     echo 'start training '
-    nohup python -u train.py --data_dir=../../data/dssm --train_dir=./model >& d2v2.log  &
-fi
-
-if [ $1 = predict ]; then
+    nohup python -u train.py --data_dir=../../data/dssm --train_dir=./model >& d2v.log  &
+elif [ $1 = train2 ]; then
+    echo 'start training2 '
+    nohup python -u train.py --data_dir=../../data/dssm2 --train_dir=./model2 --sentence_len=600 --vocab_size=437403 >& dssm2.log  &
+elif [ $1 = predict ]; then
     echo 'start predict'
-    python -u predict.py --model_dir=./model_yarn >& ttt &
-fi
-
-if [ $1 = board ]; then
-    tensorboard --logdir train_summary_yarn &
+    python -u predict.py --model_dir=./model >& ttt &
+elif [ $1 = predict2 ]; then
+    echo 'start predict2'
+    python -u predict.py --model_dir=./model2 --sentence_len=600 --vocab_path=../../data/dssm2/dssm_voc2.txt --test_path=../../data/dssm2/test.txt>& ttt &
+elif [ $1 = predict_hdfs ]; then
+    echo 'start predict hdfs'
+    python -u predict.py --model_dir=./hdfs/dssm1_model >& ttt &
+elif [ $1 = predict_hdfs2 ]; then
+    echo 'start predict hdfs'
+    python -u predict.py --model_dir=./hdfs/dssm2_model --sentence_len=600 --vocab_path=../../data/dssm2/dssm_voc2.txt --test_path=../../data/dssm2/test.txt >& ttt &
+elif [ $1 = board ]; then
+    tensorboard --logdir ./model &
 fi
 
 
